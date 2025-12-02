3b8dd5c252001235abe1c38fa6aa29acce361259
Dor-J <Mr.DorJosef@gmail.com>
2025-12-02 13:34:22 +0200

    feat: add reactive form and save output for todo creation

--- frontend/src/app/app/components/todo/todo-form/todo-form.ts
+++ frontend/src/app/app/components/todo/todo-form/todo-form.ts
@@ -1,11 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-todo-form',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './todo-form.html',
 })
 export class TodoForm {
+  @Output() save = new EventEmitter<{ title: string; description?: string; isCompleted?: boolean }>();
 
+  readonly form = this.fb.nonNullable.group({
+    title: ['', [Validators.required, Validators.maxLength(120)]],
+    description: [''],
+    isCompleted: [false],
+  });
+
+  constructor(private readonly fb: FormBuilder) {}
+
+  handleSubmit(): void {
+    if (this.form.invalid) {
+      this.form.markAllAsTouched();
+      return;
+    }
+    this.save.emit(this.form.getRawValue());
+    this.form.reset({ title: '', description: '', isCompleted: false });
+  }
 }
