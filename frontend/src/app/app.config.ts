0536cd6af659a9611912db4290d878cea38e79b2
Dor-J <Mr.DorJosef@gmail.com>
2025-12-02 12:21:41 +0200

    Refactor app.config.ts to improve import organization and include NgxDotenvModule for environment variable management - that .env files mmm

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,11 +1,17 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import {
+  ApplicationConfig,
+  importProvidersFrom,
+  provideBrowserGlobalErrorListeners,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { NgxDotenvModule } from 'ngx-dotenv';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
-  ]
+    provideRouter(routes),
+    importProvidersFrom(NgxDotenvModule.forRoot({ path: '.env' })),
+  ],
 };
