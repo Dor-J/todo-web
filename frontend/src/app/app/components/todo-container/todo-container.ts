52e3caff1d7ceaa0208ada5895269a64d401ad96
Dor-J <Mr.DorJosef@gmail.com>
2025-12-02 12:30:25 +0200

    Add app components: AppShell, ConfirmDialog, ErrorBanner, FooterBar, HeaderBar, LoadingOverlay, Toast, TodoContainer, TodoFilters, TodoForm, TodoListItem, TodoList, TodoStats with corresponding styles and tests using: ng g c app/components/app-shell ng g c app/components/header-bar ng g c app/components/footer-bar ng g c app/components/todo-container ng g c app/components/loading-overlay ng g c app/components/error-banner ng g c app/components/todo/todo-filters ng g c app/components/todo/todo-stats ng g c app/components/todo/todo-list ng g c app/components/todo/todo-list-item ng g c app/components/todo/todo-form ng g c app/components/confirm-dialog ng g c app/components/toast

---
+++ frontend/src/app/app/components/todo-container/todo-container.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-todo-container',
+  imports: [],
+  templateUrl: './todo-container.html',
+  styleUrl: './todo-container.css',
+})
+export class TodoContainer {
+
+}
---
+++ frontend/src/app/app/components/todo/todo-filters/todo-filters.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-todo-filters',
+  imports: [],
+  templateUrl: './todo-filters.html',
+  styleUrl: './todo-filters.css',
+})
+export class TodoFilters {
+
+}
