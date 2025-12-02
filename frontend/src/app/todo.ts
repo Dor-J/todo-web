425fdc8a8a50956f0cf54ea507a6475d0c6ac355
Dor-J <Mr.DorJosef@gmail.com>
2025-12-02 12:20:14 +0200

    Refactor TodoService to manage API interactions for todos

--- frontend/src/app/todo.ts
+++ frontend/src/app/todo.ts
@@ -3,6 +3,26 @@ import { Injectable } from '@angular/core';
 @Injectable({
   providedIn: 'root',
 })
-export class Todo {
-  
+export class TodoService {
+  private readonly apiUrl: string;
+
+  constructor(private http: HttpClient, private dotenv: ConfigService) {
+    this.apiUrl = this.dotenv.get('API_URL') ?? '';
+  }
+
+  getTodos(): Observable<Todo[]> {
+    return this.http.get<Todo[]>(this.apiUrl);
+  }
+
+  addTodo(todo: Omit<Todo, 'id'>): Observable<Todo> {
+    return this.http.post<Todo>(this.apiUrl, todo);
+  }
+
+  updateTodo(todo: Todo): Observable<void> {
+    return this.http.put<void>(`${this.apiUrl}/${todo.id}`, todo);
+  }
+
+  deleteTodo(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/${id}`);
+  }
 }
