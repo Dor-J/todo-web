c2c2311e004d3572cb8971c5e52eceadaaf95a98
Dor-J <Mr.DorJosef@gmail.com>
2025-12-02 13:35:06 +0200

    feat: add todo inputs and event emitters for list interactions

--- frontend/src/app/app/components/todo/todo-list/todo-list.ts
+++ frontend/src/app/app/components/todo/todo-list/todo-list.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { TodoListItem } from '../todo-list-item/todo-list-item';
+import type { Todo } from '../../../../todo';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-todo-list',
   standalone: true,
-  imports: [TodoListItem],
+  imports: [CommonModule, TodoListItem],
   templateUrl: './todo-list.html',
 })
-export class TodoList {}
+export class TodoList {
+  @Input() todos: Todo[] = [];
+  @Output() toggleTodo = new EventEmitter<Todo>();
+  @Output() editTodo = new EventEmitter<Todo>();
+  @Output() deleteTodo = new EventEmitter<string>();
+}
