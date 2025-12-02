bda5725c59f08d7dec165e17fbf1dd2a61f1e53d
Dor-J <Mr.DorJosef@gmail.com>
2025-12-02 12:44:49 +0200

    Implement confirm dialog component with structured HTML layout and Angular configuration

--- frontend/src/app/app/components/confirm-dialog/confirm-dialog.ts
+++ frontend/src/app/app/components/confirm-dialog/confirm-dialog.ts
@@ -2,9 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-confirm-dialog',
+  standalone: true,
   imports: [],
   templateUrl: './confirm-dialog.html',
-  styleUrl: './confirm-dialog.css',
 })
 export class ConfirmDialog {
 
