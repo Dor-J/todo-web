1d2f65381b282564c5291010aa403a80f2f94ba4
Dor-J <Mr.DorJosef@gmail.com>
2025-12-02 12:38:20 +0200

    Implement loading overlay component with updated HTML structure and correct Angular component configuration with tailwind

--- frontend/src/app/app/components/loading-overlay/loading-overlay.ts
+++ frontend/src/app/app/components/loading-overlay/loading-overlay.ts
@@ -2,9 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-loading-overlay',
+  standalone: true,
   imports: [],
   templateUrl: './loading-overlay.html',
-  styleUrl: './loading-overlay.css',
 })
 export class LoadingOverlay {
 
