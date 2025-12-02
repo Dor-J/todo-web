5a9e74a6425235861497d8e5d2c0b901ea167c20
Dor-J <Mr.DorJosef@gmail.com>
2025-12-02 12:45:13 +0200

    Implement todo container component with structured HTML layout and Angular configuration

--- frontend/src/app/app/components/todo-container/todo-container.ts
+++ frontend/src/app/app/components/todo-container/todo-container.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
+import { LoadingOverlay } from '../loading-overlay/loading-overlay';
+import { ErrorBanner } from '../error-banner/error-banner';
+import { TodoFilters } from '../todo/todo-filters/todo-filters';
+import { TodoStats } from '../todo/todo-stats/todo-stats';
+import { TodoForm } from '../todo/todo-form/todo-form';
+import { TodoList } from '../todo/todo-list/todo-list';
 
 @Component({
   selector: 'app-todo-container',
-  imports: [],
+  standalone: true,
+  imports: [LoadingOverlay, ErrorBanner, TodoFilters, TodoStats, TodoForm, TodoList],
   templateUrl: './todo-container.html',
-  styleUrl: './todo-container.css',
 })
-export class TodoContainer {
-
-}
+export class TodoContainer {}
