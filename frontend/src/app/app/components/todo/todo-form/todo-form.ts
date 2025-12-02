7352eb3e2643beddaa2448ab3b3a103a8a027827
Dor-J <Mr.DorJosef@gmail.com>
2025-12-02 12:44:06 +0200

    Implement todo form component with structured HTML layout and Angular configuration

--- frontend/src/app/app/components/todo/todo-form/todo-form.ts
+++ frontend/src/app/app/components/todo/todo-form/todo-form.ts
@@ -2,9 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-todo-form',
+  standalone: true,
   imports: [],
   templateUrl: './todo-form.html',
-  styleUrl: './todo-form.css',
 })
 export class TodoForm {
 
