8e6c36f8dd3f268a19a0115a659768d23c8bd82c
Dor-J <Mr.DorJosef@gmail.com>
2025-12-02 13:35:36 +0200

    feat: add todo input and action outputs on list item

--- frontend/src/app/app/components/todo/todo-list-item/todo-list-item.ts
+++ frontend/src/app/app/components/todo/todo-list-item/todo-list-item.ts
@@ -1,11 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import type { Todo } from '../../../../todo';
 
 @Component({
   selector: 'app-todo-list-item',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './todo-list-item.html',
 })
 export class TodoListItem {
-
+  @Input({ required: true }) todo!: Todo;
+  @Output() toggle = new EventEmitter<void>();
+  @Output() edit = new EventEmitter<void>();
+  @Output() delete = new EventEmitter<void>();
 }
