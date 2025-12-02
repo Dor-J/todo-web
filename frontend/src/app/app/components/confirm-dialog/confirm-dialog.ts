a0cce3ed06d02ded031eb9271933778ef188e6e7
Dor-J <Mr.DorJosef@gmail.com>
2025-12-02 13:36:49 +0200

    feat: add visible/message inputs and confirm/cancel outputs

--- frontend/src/app/app/components/confirm-dialog/confirm-dialog.ts
+++ frontend/src/app/app/components/confirm-dialog/confirm-dialog.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-confirm-dialog',
@@ -7,5 +7,10 @@ import { Component } from '@angular/core';
   templateUrl: './confirm-dialog.html',
 })
 export class ConfirmDialog {
-
+  @Input() visible = false;
+  @Input() message = 'Are you sure you want to proceed?';
+  @Input() confirmLabel = 'Delete';
+  @Input() cancelLabel = 'Cancel';
+  @Output() confirm = new EventEmitter<void>();
+  @Output() cancel = new EventEmitter<void>();
 }
