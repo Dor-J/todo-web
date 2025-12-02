e3d01abe788fd038d0dbf65130cd3a389c6307b2
Dor-J <Mr.DorJosef@gmail.com>
2025-12-02 12:45:19 +0200

    Implement toast component with structured HTML layout and Angular configuration

--- frontend/src/app/app/components/toast/toast.ts
+++ frontend/src/app/app/components/toast/toast.ts
@@ -2,9 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-toast',
+  standalone: true,
   imports: [],
   templateUrl: './toast.html',
-  styleUrl: './toast.css',
 })
 export class Toast {
 
