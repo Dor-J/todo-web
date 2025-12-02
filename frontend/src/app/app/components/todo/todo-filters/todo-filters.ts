71a6d5fa3fc5e016414f6ad8fcd750f84c99888b
Dor-J <Mr.DorJosef@gmail.com>
2025-12-02 13:32:40 +0200

    feat: add filter inputs/outputs for query and status

--- frontend/src/app/app/components/todo/todo-filters/todo-filters.ts
+++ frontend/src/app/app/components/todo/todo-filters/todo-filters.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { TodoFiltersState, TodoFilterStatus } from '../../../../models/filters';
 
 @Component({
   selector: 'app-todo-filters',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './todo-filters.html',
 })
 export class TodoFilters {
+  @Input() filters: TodoFiltersState = { query: '', status: 'all' };
+  @Output() filtersChange = new EventEmitter<TodoFiltersState>();
 
+  updateQuery(query: string): void {
+    this.filtersChange.emit({ ...this.filters, query });
+  }
+
+  setStatus(status: TodoFilterStatus): void {
+    this.filtersChange.emit({ ...this.filters, status });
+  }
 }
