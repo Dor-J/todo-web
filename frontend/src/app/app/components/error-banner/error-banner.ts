eba94d10c88917355f54d95caf5694dfab6fbfc2
Dor-J <Mr.DorJosef@gmail.com>
2025-12-02 13:32:04 +0200

    feat: accept message input and dismiss output on error banner

--- frontend/src/app/app/components/error-banner/error-banner.ts
+++ frontend/src/app/app/components/error-banner/error-banner.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-error-banner',
@@ -7,5 +7,6 @@ import { Component } from '@angular/core';
   templateUrl: './error-banner.html',
 })
 export class ErrorBanner {
-
+  @Input() message: string | null = null;
+  @Output() dismiss = new EventEmitter<void>();
 }
