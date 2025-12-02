5335d6e5b74a662c1e65c45155fa80e193031341
Dor-J <Mr.DorJosef@gmail.com>
2025-12-02 12:43:13 +0200

    Implement todo stats component with structured HTML layout and Angular configuration

--- frontend/src/app/app/components/todo/todo-stats/todo-stats.ts
+++ frontend/src/app/app/components/todo/todo-stats/todo-stats.ts
@@ -2,9 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-todo-stats',
+  standalone: true,
   imports: [],
   templateUrl: './todo-stats.html',
-  styleUrl: './todo-stats.css',
 })
 export class TodoStats {
 
