63761164685423519dce7533dd9f137bfbebee24
Dor-J <Mr.DorJosef@gmail.com>
2025-12-02 10:58:06 +0200

    Add TodoUpdateDto class for updating existing todo items

---
+++ backend/Dtos/TodoUpdateDto.cs
@@ -0,0 +1,8 @@
+namespace backend.Dtos;
+
+public class TodoUpdateDto
+{
+    public string Title { get; set; } = default!;
+    public bool IsCompleted { get; set; }
+    public string Description { get; set; }
+}
