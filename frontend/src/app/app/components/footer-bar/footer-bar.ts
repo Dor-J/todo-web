ba45f71f4d26b032b8ec970052ac2195e71f5245
Dor-J <Mr.DorJosef@gmail.com>
2025-12-02 12:41:50 +0200

    Implement footer bar component with structured HTML layout and Angular configuration

--- frontend/src/app/app/components/footer-bar/footer-bar.ts
+++ frontend/src/app/app/components/footer-bar/footer-bar.ts
@@ -2,10 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-footer-bar',
+  standalone: true,
   imports: [],
   templateUrl: './footer-bar.html',
-  styleUrl: './footer-bar.css',
 })
 export class FooterBar {
-
+  readonly year = new Date().getFullYear();
 }
